0d7e58a93f89d135d0c0b2c5b56466d5c202e4b5
Alokcsit <swainalokkumar25@gmail.com>
2024-07-22 00:49:27 +0530

    intermediate commit

--- dayfour/task1.js
+++ dayfour/task1.js
@@ -3,3 +3,42 @@
 // Activity 1: For Loop
 
 // . Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+let n =10;
+for(i=1;i<=10;i++){
+    console.log(i);
+}
+//// . Task 2: Write a program to print the multiplication table of 5 using a for loop.
+let nu=10
+ console.log("Multiplication Table of 5:")
+for(i=1;i<=nu;i++){
+    console.log("5*"+i+"="+5*i)
+}
+//// Activity 2: While Loop
+
+// . Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let num =1
+let sum =0
+while(num<=10){
+    sum+=num
+    num++
+}
+
+console.log("Sum of numbers from 1 to 10: "+sum)
+// . Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+let num1=10
+while(num1<=10&&num1>=1){
+    console.log(num1);
+    num1--
+}
+// Activity 3: Do ... While Loop
+
+// . Task 5: Write a program to print numbers from 1 to 5 using a do ... while loop.
+
+let num2=1
+do{
+    console.log(num2);
+    num2++
+}while(num2<=5)
+
+// . Task 6: Write a program to calculate the factorial of a number using a do ... while loop.
+

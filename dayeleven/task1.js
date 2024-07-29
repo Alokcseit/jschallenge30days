80d87c0a1b85e761f75b4e236af09b4203336bc5
Alokcsit <swainalokkumar25@gmail.com>
2024-07-30 00:58:10 +0530

    initial commit

--- dayeleven/task1.js
+++ dayeleven/task1.js
@@ -7,3 +7,24 @@ const promise =new Promise((resolve,reject)=>{
 
 })
 promise.then((messsage)=> console.log(messsage))
+//• Task 2: Create a promise that rejects with an error message after a 2-second timeout and handle the error using •catch() .
+const promise1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject("Promise Rejected")
+    },2000)
+})
+promise1.catch((error)=> console.log(error))
+//Activity 2: Chaining Promises
+//• Task 3: Create a sequence of promises that simulate fetching data from a server. Chain the promises to log messages in a specific order.
+ const promise2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Data fetched from server 1")
+    },1000)
+})
+const promise3=promise2.then((message)=> new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(message+" Data fetched from server 2")
+    },1000)
+}))
+
+promise3.then((message)=> console.log(message))
